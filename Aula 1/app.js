88c8c7d249e769cbf3e2b9becfb3168e30462937
Raphael Bandeira <75698406+Raphael-Bnd@users.noreply.github.com>
2025-02-02 20:57:10 -0300

    Delete Aula 1 directory

--- Aula 1/app.js
+++
@@ -1,7 +0,0 @@
-console.log("Olá, 'Mundo!'");
-console.log('Olá, "Mundo!"');
-console.log(`'Olá,' "Mundo!"`);
-
-console.log(12, 15.85, `Luiz`);
-console.log(12, 17.85, `Arthur`);
-console.log(12, 17.85, `Kaua`);
