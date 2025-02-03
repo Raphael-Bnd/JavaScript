2e718ae4f1b603a78b79344582be24f5f58c1762
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-03 01:15:35 -0300

    add func

---
+++ Tests/Function.js
@@ -0,0 +1,18 @@
+console.log(soma(34, 45))
+console.log(soma(90, 54))
+
+function soma(num1, num2) {
+    return num1 + num2
+}
+
+console.log()
+
+console.log(mult(25, 48))
+console.log(mult(5, 27))
+
+function mult(num1, num2) {
+    return num1 * num2
+}
+
+const dividindo = mult(5, 8) / 4
+console.log(dividindo)
