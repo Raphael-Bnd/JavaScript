3a011739f5c8a40d765feb17f712a38fd5b2e7c3
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-11 11:01:28 -0300

    add

---
+++ CursoJS/Aula3/tests.js
@@ -0,0 +1,5 @@
+console.log('Olá mundo');
+console.log('João nasceu em 2005');
+console.log('Em 2012 joão conheceu maria');
+console.log('Maria teve 1 filho com João em 2022');
+console.log('O filho de joão se chama Paulo');
