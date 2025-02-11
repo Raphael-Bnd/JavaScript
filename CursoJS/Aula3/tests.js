aceb992f04248a1aa232c14e4d82a7f5501fbb9d
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-11 11:21:26 -0300

    add variables

--- CursoJS/Aula3/tests.js
+++ CursoJS/Aula3/tests.js
@@ -1,6 +1,19 @@
+let marido = 'João';
+let esposa = 'Maria';
+let idade = 78;
+let filho = 'Paulo';
+let idadeFilho = 12;
+let naoDefinido; // apenas declarar a variavel
+naoDefinido = 20; // aplicando valor a variavel que foi declarada anteriormente
+
 console.log('Olá mundo');
-console.log('João nasceu em 2005');
-console.log('Em 2012 joão conheceu maria');
-console.log('Maria teve 1 filho com João em 2022');
-console.log('O filho de joão se chama Paulo');
-console.log('João faleceu aos 79 anos.');
+console.log(marido, 'nasceu em 2005.');
+console.log('Em 2012',marido, 'conheceu', esposa);
+console.log(esposa, 'teve 1 filho com', marido, 'em 2022.');
+console.log('O filho de',marido, 'se chama', filho);
+console.log('O', filho, 'tem', idadeFilho, 'anos.');
+console.log(marido, 'faleceu aos', idade, 'anos.');
+console.log(naoDefinido);
+
+naoDefinido = 50; // variavel modificada(passa a ter esse valor)
+console.log(naoDefinido);
