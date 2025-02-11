84e6d2529882752a750b871edde015bf9731fe9e
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-11 11:06:53 -0300

    add, line

--- CursoJS/Aula3/tests.js
+++ CursoJS/Aula3/tests.js
@@ -3,3 +3,4 @@ console.log('João nasceu em 2005');
 console.log('Em 2012 joão conheceu maria');
 console.log('Maria teve 1 filho com João em 2022');
 console.log('O filho de joão se chama Paulo');
+console.log('João faleceu aos 79 anos.');
