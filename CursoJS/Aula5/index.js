5062d8ec65abdd2226f2ab7acad8ac6a337cb403
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-12 00:00:26 -0300

    add while

---
+++ CursoJS/Aula5/index.js
@@ -0,0 +1,18 @@
+let i = 0
+const step = 2; 
+
+while (i < 10){
+    i++;
+    console.log(i);
+}
+
+while (i > 0){
+    i -= step;
+    console.log(i);
+}
+
+console.log();
+
+const num1 = 10;
+const num2 = parseFloat('8.5');
+console.log(num1 * num2);
