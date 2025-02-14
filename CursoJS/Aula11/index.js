e58b517ecff304e52147e9b1f6c73e03c713f826
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-13 22:35:25 -0300

    add math

---
+++ CursoJS/Aula11/index.js
@@ -0,0 +1,20 @@
+let num1 = 9.6234;
+let num5 = 36;
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+let num2 = Math.round(num1);
+console.log(num2);
+
+console.log(100/0);
+
+console.log(num5 ** (1/2));
+
+console.log(Math.max(1,2,3,4,6,-10,-50,1500,8,9,7));
+console.log(Math.min(1,2,3,4,6,-10,-50,1500,8,9,7));
+
+console.log(Math.PI);
+console.log(Math.pow(2, 10)); // dois elevado a 10
+// console.log(2 ** 10);
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(aleatorio);
