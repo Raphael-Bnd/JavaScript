05eb64ac425718a6d6e77419c6a9f8795bcf6d49
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-20 08:02:16 -0300

    compare operators

---
+++ CursoJS/logicaProgramacao/Aula19/index.js
@@ -0,0 +1,24 @@
+/*
+> maior que
+>= maior ou igual que
+< menor que
+<= menor ou igual que
+== igualdade (valor)
+=== igualdade estrita(valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+const compMaior = 10 > 5;
+const compMaiorIgual = 10 >= 5;
+console.log(compMaior);
+console.log(compMaiorIgual);
+
+const compMenor = 10 < 5;
+const compMenorIgual = 10 <= 5;
+console.log(compMenor);
+console.log(compMenorIgual);
+
+const num1 = 10;
+const num2 = 10;
+const compIgual = num1 == num2;
+console.log(compIgual);
