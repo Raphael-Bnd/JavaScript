718864ae36ca82554e8d89e316a55f21f918cf0e
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-22 14:39:52 -0300

    making changes

--- IMC-EXERCISE/assets/js/main.js
+++ IMC-EXERCISE/assets/js/main.js
@@ -3,4 +3,11 @@ const form = document.querySelector(".form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   console.log("Evento previnido");
+  setResult("Olá mundo");
+  document.querySelector(".resultado").style.display = "block";
 });
+
+function setResult(msg) {
+  const resultado = document.querySelector(".resultado");
+  resultado.innerHTML = msg;
+}
