278320773b6142f8969ae9344ee3719f153c4d71
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-22 15:25:02 -0300

    IMC

--- IMC-EXERCISE/assets/js/main.js
+++ IMC-EXERCISE/assets/js/main.js
@@ -2,12 +2,35 @@ const form = document.querySelector(".form");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("Evento previnido");
-  setResult("Olá mundo");
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+
+  if (!peso) {
+    setResult("Peso inválido", false);
+    return;
+  }
+
+  if (!altura) {
+    setResult("Altura inválido", false);
+    return;
+  }
+
   document.querySelector(".resultado").style.display = "block";
 });
 
-function setResult(msg) {
+function createP() {
+  const p = document.createElement("p");
+  return p;
+}
+
+function setResult(msg, isValid) {
   const resultado = document.querySelector(".resultado");
-  resultado.innerHTML = msg;
+  // resultado.innerHTML = msg;
+  resultado.innerHTML = "";
+  const p = createP();
+  p.innerHTML = msg;
+  resultado.appendChild(p);
 }
