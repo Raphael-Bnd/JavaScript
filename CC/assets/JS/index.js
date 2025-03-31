0b1bfcfc5443872670353f315f32e9376f945f73
Raphael Bandeira <1contatoraphael@gmail.com>
2025-03-31 17:46:55 -0300

    some changes

---
+++ CC/assets/JS/index.js
@@ -0,0 +1,9 @@
+// PREGUIÇA DE FAZER O SCRIPT DE JS
+document.addEventListener("DOMContentLoaded", function () {
+  var teste = document.getElementById("Botao");
+  if (teste) {
+    teste.addEventListener("click", function () {
+      alert("Seu cartão não foi clonado!");
+    });
+  }
+});
