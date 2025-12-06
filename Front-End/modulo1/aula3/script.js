e6596ee235bf1ca8e46fca327995b87e91d060e2
Raphael Bandeira <1contatoraphael@gmail.com>
2025-12-05 21:57:44 -0300

    adding local prettier + dropdown menu w/ jquery

--- Front-End/modulo1/aula3/script.js
+++ Front-End/modulo1/aula3/script.js
@@ -2,37 +2,36 @@ const itens = $('.item-lista');
 
 // Percorre todos os elementos da lista com a classe "item-lista"
 for (let item = 0; item < itens.length; item++) {
-
-    //Para cada elemento, imprime no console o seu conteúdo HTML
-    console.log($('.item-lista').eq(item).html()); 
+  //Para cada elemento, imprime no console o seu conteúdo HTML
+  console.log($('.item-lista').eq(item).html());
 }
 
 //Altera o conteúdo da div com id "div-one"
-$('#div-one').html('Valor da div via Jquery.'); 
+$('#div-one').html('Valor da div via Jquery.');
 
 //Imprime no console o conteúdo da div com id "div-one"
-console.log($('#div-one').html()); 
+console.log($('#div-one').html());
 
 //Imprime no console o conteúdo do elemento "li" que não possui a classe "item-lista"
-console.log($('li:not(.item-lista)').html()); 
+console.log($('li:not(.item-lista)').html());
 
 //Adiciona um novo item no final da lista com id "container-lista"
-$('#container-lista').append('<li>Quinto Item</li>'); 
+$('#container-lista').append('<li>Quinto Item</li>');
 
 //Adiciona um parágrafo após cada elemento "li"
-$('li').after('<p>texto inserido após cada tag "Li"</p>'); 
+$('li').after('<p>texto inserido após cada tag "Li"</p>');
 
 //Adiciona um título antes da lista com id "container-lista"
-$('#container-lista').before('<h3>Subtitulo da lista não ordenada</h3>'); 
+$('#container-lista').before('<h3>Subtitulo da lista não ordenada</h3>');
 
 //Remove o último parágrafo adicionado após os itens da lista
-$('p').eq(-1).remove(); 
+$('p').eq(-1).remove();
 
 //Remove todos os itens da lista que possuem a classe "item-lista"
-// $('li.item-lista').remove(); 
+// $('li.item-lista').remove();
 
 //Sobrescrevendo o conteúdo da primeira tag <p>
-$( 'p' ).first().html("Esse texto sobrescreve o inicial.");
+$('p').first().html('Esse texto sobrescreve o inicial.');
 
 //Adicionando um novo conteúdo ao conteúdo já existente
 //Foi utilizado o seletor first() para selecionar o primeiro elemento <p>
@@ -41,7 +40,7 @@ $( 'p' ).first().html("Esse texto sobrescreve o inicial.");
 //Uma outra forma de adicionar um novo conteúdo junto ao conteúdo já existente.
 //Foi utilizado o seletor last() para selecionar o último elemento <p>
 //$( 'p' ).last().html( $( 'p' ).last().html() + "Esse texto é adicionado ao final do texto existente." );
-		
+
 //Removendo o conteúdo da tag <span>
 //Foi utilizado o seletor find() para localizar os elementos <span> dentro dos elementos <p>
 //$( 'p' ).find('span').html('');
---
+++ Front-End/modulo1/aula4/script.js
@@ -0,0 +1,23 @@
+//Alerta ao carregar a página usando JavaScript puro
+// window.onload = function () {
+//     alert('Evento disparado ao final do carregamento da página!');
+// };
+
+//Alerta ao carregar a página usando jQuery
+$(function () {
+  alert('Evento disparado com Jquery ao final do carregamento da página!');
+});
+
+$('#botao-inicial').on('click', function () {
+  if ($('#texto-scnd').length) {
+    $('#texto-scnd').slideUp(300, function () {
+      $(this).remove();
+    });
+  } else {
+    var novoParagrafo = $(
+      '<p id="texto-scnd" class="texto">Paragrafo que foi adicionado ao clicar no botão com Jquery.</p>'
+    );
+    $('.parent').append(novoParagrafo);
+    novoParagrafo.hide().slideDown(500);
+  }
+});
