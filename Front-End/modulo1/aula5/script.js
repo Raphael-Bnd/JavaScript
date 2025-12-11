ad7e8f0a9bb3fe04803f5afe0eded235241b51aa
Raphael Bandeira <1contatoraphael@gmail.com>
2025-12-11 20:40:43 -0300

    SO MUCH CHANGES HOW I ABBREVIATE THIS WTF!

--- Front-End/modulo1/aula5/script.js
+++ Front-End/modulo1/aula5/script.js
@@ -1,1 +1,14 @@
-  
+$(function () {
+  $('#linkAmazon').on('click', function (event) {
+    //    event.preventDefault();
+    alert('Você está saindo do site da aula5');
+  });
+});
+
+$('#linkAmazon')
+  .on('mouseover', function () {
+    $(this).addClass('projetado');
+  })
+  .on('mouseout', function () {
+    $(this).removeClass('projetado');
+  });
