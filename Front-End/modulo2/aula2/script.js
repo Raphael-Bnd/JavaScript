39fbb1e97c1b00886b7e3f8ad0dda71e9cbf0429
Raphael-Bnd <1contatoraphael@gmail.com>
2025-12-21 06:30:43 -0300

    add automatic preview of matricula, name and foto

---
+++ Front-End/modulo2/aula2/script.js
@@ -0,0 +1,18 @@
+Vue.createApp({
+  data() {
+    return {
+      nome: '',
+      matricula: '',
+      foto: null,
+    };
+  },
+  methods: {
+    onFileChange(event) {
+      const file = event.target.files[0];
+
+      if (!file) return;
+
+      this.foto = URL.createObjectURL(file);
+    },
+  },
+}).mount('#central');
